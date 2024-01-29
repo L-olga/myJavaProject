d651bb6a09322bdcaf5291b490a4e73daf7bb371
Olga <semenets.qa@gmail.com>
2024-01-29 10:51:51 +0100

    First commit

--- AQA/src/main/java/lesson1/HomeWorkApp.java
+++ AQA/src/main/java/lesson1/HomeWorkApp.java
@@ -1,7 +1,7 @@
 package lesson1;
-
+//Создать удаленный репозиторий на GitHub, подключить его к IDE в качестве проекта, добавить к нему модуль Maven. Создать новую ветку в репозитории в IntelliJ IDEA, создать класс HomeWorkApp, и прописать в нем метод main().
 public class HomeWorkApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello");
     }
 }
